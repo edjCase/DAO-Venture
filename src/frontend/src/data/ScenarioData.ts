0d92af241b91b70e3d47482385e7b190c71402bf
Gekctek <gekctek@edjcase.com>
2024-04-15 13:52:13 -0700

    Removing story scenarios

--- src/frontend/src/data/ScenarioData.ts
+++ src/frontend/src/data/ScenarioData.ts
@@ -12,13 +12,13 @@ export interface Scenario {
 
 export let scenarios: Scenario[] = [
     {
-        id: "SURGE_CRISIS_OPENING_CEREMONY",
-        title: "Surge Crisis at the Opening Ceremony",
-        description: "The DAOball opening ceremony is under threat due to an unprecedented energy surge. Teams, as beings of energy, face a dilemma: contribute their energy to stabilize the grid or conserve it for the upcoming matches. At least 4 teams need to contribute their energy to stabilize the grid.",
+        id: "ENERGY_CONTRIBUTION",
+        title: "Energy Required",
+        description: "The league needs at least 4 more energy for operations or the whole league will gain entropy.",
         options: [
             {
-                title: "Contribute",
-                description: "Contribute energy to stabilize the grid. This will give the league 1 energy but will cause sluggishness in the next match for your team.",
+                title: "Give",
+                description: "Gives the league 1 energy but will cause sluggishness in the next match for your team.",
                 effect: {
                     allOf: [
                         {
@@ -40,7 +40,7 @@ export let scenarios: Scenario[] = [
             },
             {
                 title: "Conserve",
-                description: "Conserve your energy for upcoming matches, but risk the opening ceremony not having enough power.",
+                description: "Give no energy.",
                 effect: {
                     entropy: {
                         team: { choosingTeam: null },
@@ -49,13 +49,10 @@ export let scenarios: Scenario[] = [
                 },
             },
             {
-                title: "Convince fans to reduce energy",
-                description: "Convince fans to reduce their energy consumption, at the risk of backlash. This will not affect your team's energy, but could do nothing.",
+                title: "Try Optimizing",
+                description: "Give no energy but try to reduce energy consumption, but no guarentees.",
                 effect: {
-                    energy: {
-                        team: { choosingTeam: null },
-                        value: { flat: BigInt(-1) }, // Spend tokens to convince fans
-                    }
+                    noEffect: null
                 },
             },
         ],
@@ -92,7 +89,7 @@ export let scenarios: Scenario[] = [
     {
         id: "TRAINING_BID",
         title: "Training bid",
-        description: "Training bid",
+        description: "Bid to boost your pitcher's throwing power through specialized training. The amount of training each pitcher receives will be proportional to their bid's size relative to the total bids placed.",
         options: [
             {
                 title: "No bid",
@@ -160,7 +157,7 @@ export let scenarios: Scenario[] = [
     {
         id: "TRAINING_LOTTERY",
         title: "Training lottery",
-        description: "Training lottery",
+        description: "Enter the lottery to win +3 speed training for your first baseman. The more tickets you buy, the higher your chances of winning the training boost.",
         options: [
             {
                 title: "No ticket",
@@ -226,131 +223,64 @@ export let scenarios: Scenario[] = [
         },
     },
     {
-        id: "TRAINING_SECRET_BIDDING",
-        title: "Training secret bidding",
-        description: "Training secret bidding",
+        id: "SCAVENGE",
+        title: "Scavenge",
+        description: "Scavenge for resources to improve your team.",
         options: [
             {
-                title: "No bid",
-                description: "No bid.",
+                title: "Play it safe",
+                description: "Don't scavenge, can't risk it.",
                 effect: { noEffect: null },
             },
             {
-                title: "Bid 1",
-                description: "Bid 1 energy",
+                title: "Scavenge",
+                description: "Scavenge for energy and knowledge but risk injury.",
                 effect: {
-                    energy: {
-                        team: { choosingTeam: null },
-                        value: { flat: BigInt(1) },
-                    }
-                },
-            },
-            {
-                title: "Bid 2",
-                description: "Bid 2 energy",
-                effect: {
-                    energy: {
-                        team: { choosingTeam: null },
-                        value: { flat: BigInt(2) },
-                    }
-                },
-            },
-            {
-                title: "Bid 3",
-                description: "Bid 3 energy",
-                effect: {
-                    energy: {
-                        team: { choosingTeam: null },
-                        value: { flat: BigInt(3) },
-                    }
-                },
-            },
-        ],
-        metaEffect: {
-            proportionalBid: {
-                prize: {
-                    skill: {
-                        skill: { throwingPower: null },
-                        target: { position: { pitcher: null } },
-                        duration: { indefinite: null },
-                        total: BigInt(10),
-                    }
-                },
-                options: [
-                    {
-                        bidValue: BigInt(0),
-                    },
-                    {
-                        bidValue: BigInt(1),
-                    },
-                    {
-                        bidValue: BigInt(2),
-                    },
-                    {
-                        bidValue: BigInt(3),
-                    },
-                ],
-            },
-        },
-    },
-    {
-        id: "RESOURCE_MANAGEMENT_CHALLENGE",
-        title: "Resource Management Challenge",
-        description: "An unexpected shortage of essential resources puts all teams in a tight spot. How will you manage?",
-        options: [
-            {
-                title: "Share Resources",
-                description: "Share your scarce resources with the league, promoting unity.",
-                effect: {
-                    allOf: [
-                        {
-                            entropy: {
-                                team: { choosingTeam: null },
-                                delta: BigInt(-1), // Decrease entropy for promoting unity
+                    oneOf: [
+                        [
+                            BigInt(1),
+                            {
+                                energy: {
+                                    team: { choosingTeam: null },
+                                    value: { flat: BigInt(2) },
+                                }
                             }
-                        },
-                        {
-                            energy: {
-                                team: { choosingTeam: null },
-                                value: { flat: BigInt(-1) }, // Spend tokens to share resources
+                        ],
+                        [
+                            BigInt(1),
+                            {
+                                energy: {
+                                    team: { choosingTeam: null },
+                                    value: { flat: BigInt(3) },
+                                }
                             }
-                        },
+                        ],
+                        [
+                            BigInt(1),
+                            {
+                                skill: {
+                                    target: { positions: [{ teamId: { choosingTeam: null }, position: { firstBase: null } }] }, // TODO randomize position
+                                    skill: { speed: null }, // TODO randomize skill
+                                    delta: BigInt(1),
+                                    duration: { indefinite: null },
+                                }
+                            }
+                        ],
+                        [
+                            BigInt(2),
+                            {
+                                injury: {
+                                    target: { positions: [{ teamId: { choosingTeam: null }, position: { firstBase: null } }] }, // TODO randomize position
+                                    injury: { brokenArm: null }, // TODO randomize injury
+                                }
+                            }
+                        ],
                     ]
                 },
             },
-            {
-                title: "Conserve Resources",
-                description: "Keep your resources, ensuring your team's stability.",
-                effect: {
-                    entropy: {
-                        team: { choosingTeam: null },
-                        delta: BigInt(1), // Increase entropy for being selfish
-                    }
-                },
-            },
         ],
         metaEffect: {
-            threshold: {
-                threshold: BigInt(4), // Total resources shared threshold
-                over: {
-                    entropy: {
-                        team: { choosingTeam: null },
-                        delta: BigInt(-1), // Additional decrease in entropy for sharing teams if threshold is met
-                    }
-                },
-                under: {
-                    entropy: {
-                        team: { choosingTeam: null },
-                        delta: BigInt(1), // Increase in entropy for all teams if threshold is not met
-                    }
-                },
-                options: [
-                    { value: { fixed: BigInt(1) } },
-                    { value: { fixed: BigInt(0) } },
-                ],
-            },
-        },
+            noEffect: null,
+        }
     }
-
-
 ];
