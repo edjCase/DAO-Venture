0b575e0c87d2a093386417f3bbaa241cc4cf83f6
Gekctek <gekctek@edjcase.com>
2024-09-20 22:16:08 -0700

    ContentGrid

--- src/frontend/src/components/ContentPage.svelte
+++ src/frontend/src/components/ContentPage.svelte
@@ -12,6 +12,7 @@
   import Race from "./content/Race.svelte";
   import Zone from "./content/Zone.svelte";
   import { zoneStore } from "../stores/ZoneStore";
+  import ContentGrid from "./content/ContentGrid.svelte";
 
   $: items = $itemStore;
   $: weapons = $weaponStore;
@@ -23,57 +24,21 @@
 
 <Tabs>
   <TabItem open title="Items">
-    <div class="flex flex-wrap gap-2 justify-between">
-      {#each items as item}
-        <div class="border">
-          <Item {item} pixelSize={2} />
-        </div>
-      {/each}
-    </div>
+    <ContentGrid {items} Component={Item} propName="item" />
   </TabItem>
   <TabItem title="Weapons">
-    <div class="flex flex-wrap gap-2 justify-between">
-      {#each weapons as weapon}
-        <div class="border">
-          <Weapon {weapon} pixelSize={2} />
-        </div>
-      {/each}
-    </div>
+    <ContentGrid items={weapons} Component={Weapon} propName="weapon" />
   </TabItem>
   <TabItem title="Classes">
-    <div class="flex flex-wrap gap-2 justify-between">
-      {#each classes as class_}
-        <div class="border">
-          <Class value={class_} pixelSize={2} />
-        </div>
-      {/each}
-    </div>
+    <ContentGrid items={classes} Component={Class} />
   </TabItem>
   <TabItem title="Creatures">
-    <div class="flex flex-wrap gap-2 justify-between">
-      {#each creatures as creature}
-        <div class="border">
-          <Creature value={creature} pixelSize={2} />
-        </div>
-      {/each}
-    </div>
+    <ContentGrid items={creatures} Component={Creature} />
   </TabItem>
   <TabItem title="Races">
-    <div class="flex flex-wrap gap-2 justify-between">
-      {#each races as race}
-        <div class="border">
-          <Race value={race} pixelSize={2} />
-        </div>
-      {/each}
-    </div>
+    <ContentGrid items={races} Component={Race} />
   </TabItem>
   <TabItem title="Zones">
-    <div class="flex flex-wrap gap-2 justify-between">
-      {#each zones as zone}
-        <div class="border">
-          <Zone value={zone} pixelSize={2} />
-        </div>
-      {/each}
-    </div>
+    <ContentGrid items={zones} Component={Zone} />
   </TabItem>
 </Tabs>
---
+++ src/frontend/src/components/content/ContentGrid.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let items: any[] | undefined;
+  export let Component: any;
+  export let propName: string = "value";
+</script>
+
+<div class="flex flex-wrap gap-2 justify-between">
+  {#if items !== undefined}
+    {#each items as item}
+      <div class="border">
+        <svelte:component
+          this={Component}
+          {...{ [propName]: item }}
+          pixelSize={2}
+        />
+      </div>
+    {/each}
+  {/if}
+</div>
