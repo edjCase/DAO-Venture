16e573cdf80ef5cecf59bf4df58c0e2111f96e1e
Gekctek <gekctek@edjcase.com>
2024-09-26 18:05:21 -0700

    Fixing

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -2,32 +2,23 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import inject from '@rollup/plugin-inject'
-import replace from '@rollup/plugin-replace'
 import sveltePreprocess from 'svelte-preprocess';
-import { initCanisterIds } from './common.config';
+import environment from 'vite-plugin-environment';
+import dotenv from 'dotenv';
 
-const production = process.env.NODE_ENV === 'production'
 
-
-const { canisterIds, network } = initCanisterIds(production)
-
-// Fallback for undefined canister IDs
-const UNDEFINED_CANISTER_IDS = {
-    "process.env.INTERNET_IDENTITY_CANISTER_ID": JSON.stringify("undefined"),
-}
+dotenv.config({ path: '../../.env' });
 
 export default defineConfig({
     root: '.', // Set the root directory of your project
     build: {
         target: 'esnext',
-        sourcemap: !production,
         outDir: 'dist', // Specify the output directory for the build
         emptyOutDir: true, // Empty the output directory before building
     },
     plugins: [
         svelte({
             preprocess: sveltePreprocess({
-                sourceMap: !production,
                 postcss: true
             }),
         }),
@@ -35,22 +26,8 @@ export default defineConfig({
         inject({
             Buffer: ['buffer', 'Buffer'],
         }),
-        replace({
-            preventAssignment: true,
-            "process.env.DFX_NETWORK": JSON.stringify(network),
-            "process.env.LOCAL_NETWORK_PORT": JSON.stringify("4943"),
-            "process.env.NODE_ENV": JSON.stringify(production ? "production" : "development"),
-            "process.env.LOG_LEVEL": JSON.stringify(production ? "info" : "debug"),
-            ...UNDEFINED_CANISTER_IDS,
-            ...Object.fromEntries(
-                Object.keys(canisterIds)
-                    .filter((canisterName) => canisterName !== "__Candid_UI")
-                    .flatMap((canisterName) => [
-                        [`process.env.${canisterName.toUpperCase()}_CANISTER_ID`, JSON.stringify(canisterIds[canisterName][network])],
-                        [`process.env.CANISTER_ID_${canisterName.toUpperCase()}`, JSON.stringify(canisterIds[canisterName][network])],
-                    ])
-            ),
-        }),
+        environment("all", { prefix: "CANISTER_" }),
+        environment("all", { prefix: "DFX_" }),
     ],
     resolve: {
         alias: {
